0287a1e6dfc3828d492fda035995c86be823ca78
Ahmed <ahmedelsharkwiti@gmail.com>
2024-02-14 22:35:29 +0200

    02-14 22:35

--- 0x01-challenge/main.py
+++ 0x01-challenge/main.py
@@ -1,23 +1,35 @@
 #!/usr/bin/python3
 """ Check response
 """
+import requests
+import sys
 
 if __name__ == "__main__":
     try:
-        from square import Square
-
-        s = Square(width=30, height=30)
-        a = s.area_of_my_square()
-        if a != (30 * 30):
-            print("Wrong area_of_my_square for 30/30: {}".format(a))
+        r = requests.get('http://0.0.0.0:5000/api/v1/status')
+        if r.status_code != 200:
+            print("Wrong status code: {}".format(r.status_code))
             exit(1)
 
-        s = Square(width=30, height=40)
-        a = s.area_of_my_square()
-        if a != (30 * 40):
-            print("Wrong area_of_my_square for 30/40: {}".format(a))
+        if r.headers.get('content-type') != "application/json":
+            print("Wrong content type: {}".format(r.headers.get('content-type')))
             exit(1)
-        
+
+        r_json = r.json()
+
+        if len(r_json.keys()) != 1:
+            print("Not the right number of element in the JSON: {}".format(r_json))
+            exit(1)
+
+        r_status = r_json.get('status')
+        if r_status is None:
+            print("Missing status key: {}".format(r_json))
+            exit(1)
+
+        if r_status != "OK":
+            print("Wrong status value: {}".format(r_status))
+            exit(1)
+
         print("OK", end="")
     except:
         print("Error: {}".format(sys.exc_info()))
